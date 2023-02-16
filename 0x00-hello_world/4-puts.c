486435706145d499cc40ca1e42ad3e8d821e9acf
Melakudesalegn <melakudesa123@gmail.com>
2023-02-16 14:02:36 -0800

    first priograming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 /*
- * main - Entry point
+ * main - print line
+ *
  * Return: Always 0 
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+        puts("\"Programming is like building a multilingual puzzle");
+        return 0;
+}       
+~                                                                                                                                                    
+~                                                                                                                                                    
+~                            
