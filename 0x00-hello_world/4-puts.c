3cff14488059b2972a3b8130312a6e59c61a2232
Melakudesalegn <melakudesa123@gmail.com>
2023-02-16 14:13:24 -0800

    check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*
+/**
  * main - print line
- *
  * Return: Always 0 
 */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+        return(0);
 }       
 ~                                                                                                                                                    
 ~                                                                                                                                                    
