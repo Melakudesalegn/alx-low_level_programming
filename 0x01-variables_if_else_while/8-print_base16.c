85345ee341d8297da3ffef5b84ec7be9195ad3a8
Melakudesalegn <melakudesa123@gmail.com>
2023-02-17 13:26:13 -0800

    print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-
 /**
- * main -Entry
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char c;
+
 	int d;
+	
 	c = 'a';
 	d = 0;
-
 	while
 		(d < 10){
 			putchar(d +'0');
