c051fbf9c9140d0adf7545edd299d99c49786887
Melakudesalegn <melakudesa123@gmail.com>
2023-03-10 05:28:19 -0800

    static labirary one

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ * puts: programming building
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
